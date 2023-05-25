519740d488e8426b6aaa8c438ce9d7b9f9233f45
ahmet bulutluoz <abbulutluoz@gmail.com>
2023-05-25 12:36:58 +0200

    33.gun sonu

--- src/day33_maps/C07_Compute.java
+++ src/day33_maps/C07_Compute.java
@@ -1,4 +1,75 @@
 package day33_maps;
 
+import java.util.Map;
+import java.util.TreeMap;
+
 public class C07_Compute {
+
+
+
+    public static void main(String[] args) {
+        Map<String,Integer> harfKullanimSayilari = new TreeMap<>();
+
+        harfKullanimSayilari.put("A" ,20);
+        harfKullanimSayilari.put("K" ,10);
+        harfKullanimSayilari.put("C" ,25);
+        harfKullanimSayilari.put("M" ,23);
+        harfKullanimSayilari.put("Z" ,32);
+
+        System.out.println(harfKullanimSayilari); //  {A=20, C=25, K=10, M=23, Z=32}
+
+        // A'nin degerini 2 katina cikarin
+
+        int eskiDeger= harfKullanimSayilari.get("A"); // 20
+        harfKullanimSayilari.put("A",2*eskiDeger);
+
+        System.out.println(harfKullanimSayilari); // {A=40, C=25, K=10, M=23, Z=32}
+
+        // B varsa degerini 3 artirin
+
+        if (harfKullanimSayilari.containsKey("B")){
+            eskiDeger = harfKullanimSayilari.get("B");
+            harfKullanimSayilari.put("B",eskiDeger+3);
+        }
+
+        System.out.println(harfKullanimSayilari); // {A=40, C=25, K=10, M=23, Z=32}
+
+
+        /*
+            java var olan veya olmayan key'lerin
+            value'leri uzerinde islem yapmak icin
+            ozel iki method gelistirmis
+
+            bu method'lar functional programing (Lambda) kullanarak calisir
+         */
+
+        // A'nin degerini 2 katina cikarin
+
+        harfKullanimSayilari.compute("A",(k,v)-> 2*v );
+        System.out.println(harfKullanimSayilari); // {A=80, C=25, K=10, M=23, Z=32}
+
+
+        // B varsa degerini 3 artirin
+
+        harfKullanimSayilari.computeIfPresent("B",(k,v)-> v+3);
+        harfKullanimSayilari.computeIfPresent("M",(k,v)-> v+3);
+        System.out.println(harfKullanimSayilari); // {A=80, C=25, K=10, M=26, Z=32}
+
+
+        // Eger B yoksa degeri 5 olarak ekleyin
+
+        harfKullanimSayilari.putIfAbsent("B",5);
+        harfKullanimSayilari.putIfAbsent("A",5);
+
+        System.out.println(harfKullanimSayilari); // {A=80, B=5, C=25, K=10, M=26, Z=32}
+
+
+        // Eger D yoksa degeri 5 olarak ekleyin
+
+        harfKullanimSayilari.computeIfAbsent("D", v ->5);
+        harfKullanimSayilari.computeIfAbsent("C", v ->5);
+
+        System.out.println(harfKullanimSayilari); // {A=80, B=5, C=25, D=5, K=10, M=26, Z=32}
+
+    }
 }
---
+++ src/day33_maps/C08_Compute.java
@@ -0,0 +1,36 @@
+package day33_maps;
+
+import java.util.Map;
+import java.util.TreeMap;
+
+public class C08_Compute {
+
+    public static void main(String[] args) {
+                /*
+        Soru : Verilen bir array’de kullanilan sayilari ve kacar defa kullanildigini yazdirin.
+
+        Input : {"a","b","s","a","s","c","d","b","c","a","a","s","z","k"}
+        output :a kullanimi : 4 adet
+                b kullanimi : 2 adet
+                c kullanimi : 2 adet
+                d kullanimi : 1 adet
+                k kullanimi : 1 adet
+                s kullanimi : 3 adet
+                z kullanimi : 1 adet
+
+         */
+
+        String[] harfler = {"a","b","s","a","s","c","d","b","c","a","a","s","z","k"} ;
+
+        Map<String,Integer> harfKullanimMapi = new TreeMap<>();
+
+        for (String eachHarf: harfler
+             ) {
+
+            harfKullanimMapi.computeIfPresent(eachHarf, (k,v)-> v+1 );
+            harfKullanimMapi.putIfAbsent(eachHarf,1);
+        }
+
+        System.out.println(harfKullanimMapi);
+    }
+}
